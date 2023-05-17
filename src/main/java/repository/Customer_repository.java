6c617feeb5945a8a4427c19a96d8cca50a004249
ADMIN <chinh20043008@gmail.com>
2023-05-17 20:42:53 +0700

    Initial commit

--- src/main/java/repository/Customer_repository.java
+++ src/main/java/repository/Customer_repository.java
@@ -1,10 +1,16 @@
 package repository;
 
 import entity.Customer_entity;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 
+@Repository
 public interface Customer_repository extends CrudRepository<Customer_entity,Integer> {
-
+    List<Customer_entity> findByName (String name);
+    List<Customer_entity> findByPhoneOrEmail(int phone, String Email);
+      List<Customer_entity> findByBirthdateBetween(LocalDate start, LocalDate end);
 }
